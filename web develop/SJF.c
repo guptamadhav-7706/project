ad4a1a4547f4f6eddd5d7ab306e03f9ecec23e58
guptamadhav-7706 <madhvendragupta29@gmail.com>
2025-06-28 11:59:32 +0530

    project added

---
+++ web develop/SJF.c
@@ -0,0 +1,65 @@
+#include<stdio.h>
+int main()
+{
+    int bt[20],pid[20],wt[20],tat[20],i,j,n,total=0,pos,temp;
+    float avg_wt,avg_tat;
+    printf("Enter number of process:");
+    scanf("%d",&n);
+  printf("Enter the process id of all processes");
+  for(i=0,i<n;i++)
+  {
+    scanf("%d",&pid);
+  }
+    printf("nEnter Burst Time:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d", &bt[i])    
+    }
+  
+   //sorting of burst times
+    for(i=0;i<n;i++)
+    {
+        pos=i;
+        for(j=i+1;j<n;j++)
+        {
+            if(bt[j]<bt[pos])
+                pos=j;
+        }
+  
+        temp=bt[i];
+        bt[i]=bt[pos];
+        bt[pos]=temp;
+  
+        temp=pid[i];
+        pid[i]=p[pos];
+        pid[pos]=temp;
+    }
+   
+    wt[0]=0;            
+  
+   
+    for(i=1;i<n;i++)
+    {
+        wt[i]=0;
+        for(j=0;j<i;j++)
+            wt[i]+=bt[j];
+  
+        total+=wt[i];
+    }
+  
+    avg_wt=(float)total/n;      
+    total=0;
+  
+    printf("nProcesst    Burst Time    tWaiting TimetTurnaround Time");
+    for(i=0;i<n;i++)
+    {
+        tat[i]=bt[i]+wt[i];   
+        total+=tat[i];
+        printf("np%dtt  %dtt    %dttt%d",p[i],bt[i],wt[i],tat[i]);
+    }
+  
+    avg_tat=(float)total/n;    
+    printf("nnAverage Waiting Time=%f",avg_wt);
+    printf("nAverage Turnaround Time=%fn",avg_tat);
+
+}
